10a5ea2e077a6db876d46adc77f9955e9fc23ba4
pirates19 <nicholas.ellul1@gmail.com>
2015-11-13 14:26:47 -0500

    Done, no menu

---
+++ ArrayAnalyzerSmall503/ArrayAnalyzerForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ArrayAnalyzerSmall503
+{
+    partial class frmArrayAnalyzer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(75, 47);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(120, 134);
+            this.lstBox.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(86, 210);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Generate Array";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // frmArrayAnalyzer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lstBox);
+            this.Name = "frmArrayAnalyzer";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ ArrayAnalyzerSmall503/ArrayAnalyzerForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 13-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-03
+ * This program returns the smallest nuumber in an array.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayAnalyzerSmall503
+{
+    public partial class frmArrayAnalyzer : Form
+    {
+        //global declaration
+        int[] arrayOfNumbers = new int[10];
+        Random randomNumbers = new Random();
+
+        public int SmallestNumber(int[] array)
+        {
+            //returns biggest number in array
+            int elementHolder = 10;
+            foreach (int element in array)
+            {
+                if (elementHolder > element)
+                {
+                    elementHolder = element;
+                }
+
+            }
+            return elementHolder;
+        }
+
+        public frmArrayAnalyzer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //input
+            const int NUMBER_OF_ITEMS = 10;
+            int randomizedNumber;
+            int leastNumber;
+
+            this.lstBox.Items.Clear();
+            Array.Clear(arrayOfNumbers, 0, arrayOfNumbers.Length);
+
+            //process
+            for (int counter = 0; counter < NUMBER_OF_ITEMS; counter++)
+            {
+                randomizedNumber = randomNumbers.Next(1, 10 + 1);
+                this.lstBox.Items.Add(randomizedNumber);
+                arrayOfNumbers[counter] = randomizedNumber;
+
+            }
+
+            //output
+            leastNumber = SmallestNumber(arrayOfNumbers);
+
+            MessageBox.Show(Convert.ToString(leastNumber), "The Smallest number is...");
+
+        }
+    }
+}
